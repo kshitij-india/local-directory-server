8fcb824dfcbf2f2553cdd2b6756a297738bb2a34
Kshitij Jain <jn.kshitij@gmail.com>
2018-10-22 14:40:57 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+var http= require('http');
+var fs= require('fs');
+var path= require('path');
+
+var server= http.createServer(function(req, res){
+    var filePath= `./served-directory${req.url}`;
+    if(filePath== './served-directory/')
+        filePath= './served-directory/index.html';
+
+    const extMap = {
+        '.ico': 'image/x-icon',
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.json': 'application/json',
+        '.css': 'text/css',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg',
+        '.jpeg': 'image/jpeg',
+        '.wav': 'audio/wav',
+        '.mp3': 'audio/mpeg',
+        '.svg': 'image/svg+xml',
+        '.pdf': 'application/pdf',
+        '.doc': 'application/msword'
+        };
+
+    var extName = path.extname(filePath);
+    
+    fs.readFile(filePath, function(error, data){
+        if(error){
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end("No file exists with the given path");
+        }
+        else{
+            res.writeHead(200, { 'Content-Type': extMap[extName] || 'text/plain' });
+            res.end(data);
+        }
+    });
+});
+
+exports.listen= function(port){
+    server.listen(port, function(){
+        console.log(`Directory 'served-directory' hosted on port ${port}. Naivgate to http://localhost:${port}/fileName to proceed`);
+    });
+};
+
+exports.close= function(){
+    server.close(function(){
+        console.log("Server closed");
+    });
+};
---
+++ test/server-test.js
@@ -0,0 +1,99 @@
+var request= require('request');
+var expect= require('chai').expect;
+var fs= require('fs');
+
+var server= require('../server');
+
+describe('tests for a static server serving the directory "served-dierctory"', function(){
+
+    before('start the server', function(){
+        server.listen(3001);
+    });
+
+    describe('it should serve the correct file from the directory according to path', function(){
+
+        it('should serve "file-a.txt" for the path "http://localhost:3001/file-a.txt', function(done){
+
+            request('http://localhost:3001/file-a.txt', function(error, response, body){
+
+                expect(error).to.not.exist;
+
+                fs.readFile('./served-directory/file-a.txt', function(err, data){
+
+                    expect(err).to.not.exist;
+
+                    expect(body).to.equal(data.toString());
+                    done();
+                });
+            });
+        });
+
+        it('should serve "file-b.txt" for the path "http://localhost:3001/file-b.txt', function(done){
+
+            request('http://localhost:3001/file-b.txt', function(error, response, body){
+
+                expect(error).to.not.exist;
+
+                fs.readFile('./served-directory/file-b.txt', function(err, data){
+
+                    expect(err).to.not.exist;
+
+                    expect(body).to.equal(data.toString());
+                    done();
+                });
+            });
+        });
+
+        it('should serve "file-c.txt" for the path "http://localhost:3001/sub-directory/file-c.txt', function(done){
+
+            request('http://localhost:3001/sub-directory/file-c.txt', function(error, response, body){
+
+                expect(error).to.not.exist;
+
+                fs.readFile('./served-directory/sub-directory/file-c.txt', function(err, data){
+
+                    expect(err).to.not.exist;
+
+                    expect(body).to.equal(data.toString());
+                    done();
+                });
+            });
+        });
+
+        it('should serve "index.html" for the root path "http://localhost:3001', function(done){
+
+            request('http://localhost:3001', function(error, response, body){
+
+                expect(error).to.not.exist;
+
+                fs.readFile('./served-directory/index.html', function(err, data){
+
+                    expect(err).to.not.exist;
+
+                    expect(body).to.equal(data.toString());
+                    done();
+                });
+            });
+        });
+    });
+
+    it('should serve an error message if file is not present in the directory', function(done){
+
+        request('http://localhost:3001/file-x.txt', function(error, response, body){
+
+            expect(error).to.not.exist;
+
+            expect(body).to.equal("No file exists with the given path");
+
+            fs.readFile('./served-directory/file-x.txt', function(err, data){
+
+                expect(err).to.exist;
+                done();
+            });
+        });
+    });
+
+    after('close the server', function(){
+        server.close();
+    });
+});
